687cdd7c1e94a5cf3f4f21fcd13dc4e8db567d0a
Unknown <fcf@di.ufpb.br>
2017-12-04 18:53:01 -0300

    Ultimas ediçoes Igor

--- src/globals.cpp
+++ src/globals.cpp
@@ -2,4 +2,7 @@
 
 int P, K, N, V, n;
 Graph G;
+double matG[NMAX][NMAX];
 std::vector<int> VC, RD;
+std::vector<double> RT, AL;
+
--- src/globals.h
+++ src/globals.h
@@ -4,9 +4,12 @@
 #include <cstdio>
 #include <vector>
 #include "Graph.h"
+#define NMAX 100
 
 extern int K, N, V, n;
 extern Graph G;
+extern double matG[NMAX][NMAX];
 extern std::vector<int> VC, RD;
+extern std::vector<double> RT, AL;
 
 #endif
